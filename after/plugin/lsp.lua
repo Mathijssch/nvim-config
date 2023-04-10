76c969ada82c6a141b48d845b1ac1886f8176ebb
Mathijs Schuurmans <mathijs.schuurmans@gmail.com>
2023-04-10 23:05:31 +0200

    Tweak keybindings

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -8,10 +8,10 @@ end)
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_select = lsp.defaults.cmp_mappings({
-	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select), 
+cmp_select = lsp.defaults.cmp_mappings({
+	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
 	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-	['<C-y>'] = cmp.mapping.confirm( {select = true} ), 
+	['<Tab>'] = cmp.mapping.confirm( {select = true} ),
 	['<C-Space>'] = cmp.mapping.complete(),
 })
 
