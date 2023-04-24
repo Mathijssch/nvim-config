7449d707f1ab98bb4341f5a07f54841e036eb6d1
Mathijs Schuurmans <mathijs.schuurmans@gmail.com>
2023-04-24 15:11:06 +0200

    Use vim-lua lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -5,7 +5,7 @@ lsp.on_attach(function(client, bufnr)
 end)
 
 -- (Optional) Configure lua language server for neovim
---require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 --local cmp = require('cmp')
 --local cmp_select = {behavior = cmp.SelectBehavior.Select}
 --cmp_select = lsp.defaults.cmp_mappings({
