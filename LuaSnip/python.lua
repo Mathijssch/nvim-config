5c7b2ca5ca0b3632471ce4de2c2b8b1f9b113312
Mathijs Schuurmans <mathijs.schuurmans@gmail.com>
2023-04-25 14:24:52 +0200

    Add python snippets

---
+++ LuaSnip/python.lua
@@ -0,0 +1,38 @@
+-- Two common LuaSnip abbreviations
+local ls = require("luasnip")
+local s = ls.snippet
+local i = ls.insert_node
+local sn = ls.snippet_node
+local t = ls.text_node
+local d = ls.dynamic_node
+local fmt = require("luasnip.extras.fmt").fmt
+local rep = require("luasnip.extras").rep
+
+-- Title
+local title = s(
+    {
+        trig = "_title",
+        dscr = "Draw a title line in comments",
+        regTrig = false
+    },
+    fmt(
+        [[
+        #-------------------------------------------------------------
+        # <>
+        #-------------------------------------------------------------
+    ]]
+        , { i(1) },
+        { delimiters = "<>" }
+    )
+)
+
+local namemain = s(
+    {
+        trig = "if __name",
+        dscr = "`if __name__ == '__main__'",
+        regTrig = false
+    },
+    t([[if __name__ == "__main__" ]])
+)
+
+return {title, namemain}
