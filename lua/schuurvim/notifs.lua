7ef7d318082f3e2f94480da939ebb78633967741
Mathijs Schuurmans <mathijs.schuurmans@gmail.com>
2023-05-03 09:53:47 +0200

    Fade animation notifications

--- lua/schuurvim/notifs.lua
+++ lua/schuurvim/notifs.lua
@@ -1,6 +1,8 @@
 -- Use the notify plugin for notifications if available
 local status_ok, notify = pcall(require, "notify")
-
 if not status_ok then return end
-
+vim.opt.termguicolors = true 
+notify.setup({
+    stages = "fade"
+})
 vim.notify = notify
