27a19358f2b19e287d0a09c5523d7e47052cf812
Mathijs Schuurmans <mathijs.schuurmans@gmail.com>
2023-05-05 15:57:41 +0200

    Toggle light/dark

--- lua/schuurvim/color.lua
+++ lua/schuurvim/color.lua
@@ -14,5 +14,7 @@ function DarkMode()
     SetColors("palenightfall")
 end
 
+vim.api.nvim_create_user_command("DarkMode", DarkMode, {})
+vim.api.nvim_create_user_command("LightMode", LightMode, {})
 
 vim.cmd([[au TextYankPost * silent! lua vim.highlight.on_yank {higroup="IncSearch", timeout=150}]])
