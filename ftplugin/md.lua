ab0afba5f463d55361f2fa3b1dd27b756ece8f5f
Mathijs Schuurmans <mathijs.schuurmans@gmail.com>
2024-04-04 10:12:31 +0200

    Formatting and \\lv -> `GotoPage`

--- ftplugin/md.lua
+++ ftplugin/md.lua
@@ -1,9 +1,10 @@
 -- Markdown keybindings
-vim.api.nvim_set_keymap('i', '<C-B>', [[****<Esc>hi]], {noremap = true})
-vim.api.nvim_set_keymap('i', '<C-I>', [[**<Esc>i]], {noremap = true})
-vim.api.nvim_set_keymap('v', '<C-B>', [[c**<Esc>pa**]], {noremap = true})
-vim.api.nvim_set_keymap('v', '<C-I>', [[c*<Esc>pa*]], {noremap = true})
+vim.api.nvim_set_keymap('i', '<C-B>', [[****<Esc>hi]], { noremap = true })
+vim.api.nvim_set_keymap('i', '<C-I>', [[**<Esc>i]], { noremap = true })
+vim.api.nvim_set_keymap('v', '<C-B>', [[c**<Esc>pa**]], { noremap = true })
+vim.api.nvim_set_keymap('v', '<C-I>', [[c*<Esc>pa*]], { noremap = true })
 --vim.api.nvim_set_keymap('v', '<C-b>', [[d]], {noremap = true})
 --
 --vim.api.nvim_set_keymap('v', '<C-i>', '**<Esc>hi', {noremap = true})
+vim.api.nvim_set_keymap('n', [[<localleader>lv]], [[<cmd>GotoPage<CR>]], { noremap = true })
 
