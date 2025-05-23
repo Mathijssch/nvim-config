7c5aa06824082b331ece081f4775fa38545a104e
Mathijs Schuurmans <mathijs.schuurmans@gmail.com>
2025-05-23 15:56:03 +0200

    Plugin: Remove DoGe

--- lua/plugins/doge.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "kkoomen/vim-doge",
-  keys = {
-    { "<Leader>dg", "<Plug>(doge-generate)", desc = "Generate a docstring for the function under the cursor." }
-  },
-  opts = function()
-    vim.cmd([[let g:doge_doc_standard_python = 'numpy']])
-  end,
-}
