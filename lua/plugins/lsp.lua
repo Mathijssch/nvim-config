86f5e93e6f026feaf2d2656f9a2f885ca9bf4b1c
Mathijs Schuurmans <mathijs.schuurmans@gmail.com>
2026-01-20 08:27:29 +0100

    Port to new opts structure

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,15 +2,15 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = function(_, opts)
-      local caps = vim.lsp.protocol.make_client_capabilities()
-      caps.offsetEncoding = { "utf-8" }
-      opts.capabilities = vim.tbl_deep_extend("force", opts.capabilities or {}, caps)
-
+      -- local caps = vim.lsp.protocol.make_client_capabilities()
+      -- caps.offsetEncoding = { "utf-8" }
       opts.servers = vim.tbl_deep_extend("force", opts.servers or {}, {
         -- pyright = {
         --   mason = false,
         --   autostart = false,
         -- },
+        --
+        -- capabilities = vim.tbl_deep_extend("force", opts.servers.capabilities or {}, caps),
         pylsp = {
           settings = {
             pylsp = {
